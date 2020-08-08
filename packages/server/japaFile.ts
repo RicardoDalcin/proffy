c3f4a7d638d13845a4251786e3751893901bf47d
Ricardo <ricardo.dalcinh@gmail.com>
2020-08-08 11:59:42 -0300

    Setup Japa for Adonis v5 tests

---
+++ packages/server/japaFile.ts
@@ -0,0 +1,20 @@
+import 'reflect-metadata'
+import { join } from 'path'
+import getPort from 'get-port'
+import { configure } from 'japa'
+import sourceMapSupport from 'source-map-support'
+
+process.env.NODE_ENV = 'testing'
+process.env.ADONIS_ACE_CWD = join(__dirname, '..')
+sourceMapSupport.install({ handleUncaughtExceptions: false })
+
+async function startHttpServer() {
+  const { Ignitor } = await import('@adonisjs/core/build/src/Ignitor')
+  process.env.PORT = String(await getPort())
+  await new Ignitor(__dirname).httpServer().start()
+}
+
+configure({
+  files: ['build/test/**/*.spec.js'],
+  before: [startHttpServer]
+})
---
+++ packages/server/test/example.spec.ts
@@ -0,0 +1,14 @@
+import test from 'japa'
+import { JSDOM } from 'jsdom'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+test.group('Example test', () => {
+  test('ensure root route works', async assert => {
+    /**
+     * Make request
+     */
+    const { text } = await supertest(BASE_URL).get('/').expect(200)
+  })
+})
